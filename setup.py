e4445b932efa73635428712f1ec06145cb346c2c
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-01-10 19:04:26 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TwitterFrontendFlow",
+    version="2.1",
+    description="Unofficial Client for Twitter Internal API",
+    url="https://github.com/fa0311/TwitterFrontendFlow",
+    packages=find_packages(),
+)
