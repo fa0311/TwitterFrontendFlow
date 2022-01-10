6ed629047bcdf6e5b99f9db7aec89b1cc9eecc78
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-01-10 22:35:24 +0900

    add sample.py

---
+++ sample.py
@@ -0,0 +1,76 @@
+from TwitterFrontendFlow.TwitterFrontendFlow import TwitterFrontendFlow
+
+flow = TwitterFrontendFlow()
+
+print(
+"""login: ログイン
+password_reset: パスワードリセット
+load: cookieのロード""")
+
+action = input()
+
+if action == "login":
+    flow.login_flow()
+    flow.LoginJsInstrumentationSubtask()
+    if "LoginEnterUserIdentifierSSOSubtask"in flow.get_subtask_ids():
+        print("電話番号/メールアドレス/ユーザー名")
+        flow.LoginEnterUserIdentifierSSOSubtask(input())
+    if "LoginEnterAlternateIdentifierSubtask"in flow.get_subtask_ids():
+        print(flow.content["subtasks"][0]["enter_text"]["primary_text"]["text"])
+        flow.LoginEnterAlternateIdentifierSubtask(input())
+    if "LoginEnterPassword"in flow.get_subtask_ids():
+        print(flow.content["subtasks"][0]["enter_password"]["primary_text"]["text"])
+        flow.LoginEnterPassword(input())
+    if "LoginTwoFactorAuthChallenge"in flow.get_subtask_ids():
+        print(flow.content["subtasks"][0]["enter_text"]["header"]["primary_text"]["text"])
+        flow.LoginTwoFactorAuthChallenge(input())
+    if "LoginSuccessSubtask"in flow.get_subtask_ids():
+        print("ログインしました")
+
+elif action == "password_reset":
+    flow.password_reset_flow()
+    flow.PwrJsInstrumentationSubtask()
+    if "PasswordResetBegin"in flow.get_subtask_ids():
+        print("電話番号/メールアドレス/ユーザー名")
+        flow.PasswordResetBegin(input())
+    if "PwrKnowledgeChallenge"in flow.get_subtask_ids():
+        print(flow.content["subtasks"][0]["enter_text"]["secondary_text"]["text"])
+        flow.PwrKnowledgeChallenge(input())
+    if "PwrKnowledgeChallenge"in flow.get_subtask_ids():
+        print(flow.content["subtasks"][0]["enter_text"]["secondary_text"]["text"])
+        flow.PwrKnowledgeChallenge(input())
+    if "PasswordResetChooseChallenge"in flow.get_subtask_ids():
+        print("\n".join([choices["id"] + ": " + choices["text"]["text"] for choices in flow.content["subtasks"][0]["choice_selection"]["choices"]]))
+        flow.PasswordResetChooseChallenge(input())
+    if "PasswordResetConfirmChallenge"in flow.get_subtask_ids():
+        print("コードを入力")
+        flow.PasswordResetConfirmChallenge(input())
+    if "PasswordResetNewPassword"in flow.get_subtask_ids():
+        print("新しいパスワードを入力")
+        flow.PasswordResetNewPassword(input())
+    if "PasswordResetSurvey"in flow.get_subtask_ids():
+        print("パスワードを変更した理由を教えてください")
+        print("\n".join([choices["id"] + ": " + choices["text"]["text"] for choices in flow.content["subtasks"][0]["choice_selection"]["choices"]]))
+        flow.PasswordResetSurvey(input())
+    exit()
+
+elif action == "load":
+    print("ファイル名")
+    flow.LoadCookies(input())
+
+while True:
+    print(
+"""tweet: ツイート
+save: cookieの出力
+end: 終了""")
+
+    action = input()
+
+    if action == "tweet":
+        print("ツイート内容")
+        flow.CreateTweet(input())
+    elif action == "save":
+        print("ファイル名")
+        flow.SaveCookies(input())
+    elif action == "end":
+        break
